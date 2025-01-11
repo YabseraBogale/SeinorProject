a2b534a25a48a48fa452802935562a836746c380
YabseraBogale <yabserapython@gmail.com>
2025-01-11 13:51:34 +0300

    ok

--- test.py
+++ test.py
@@ -7,52 +7,40 @@ from Database.rating import Rating
 from Database.buyer import Buyer
 import bcrypt
 
-# print(Database().AllowedFile("filename.png"))
-# print(Database().AllowedFile("filename.jpg"))
-# print(Database().AllowedFile("filename.jpeg"))
-# print(Database().AllowedFile("filename.txt"))
-# print(AddisCheretaUser().ResetPassWordForUser('getahunababu@gmail.com','654321'))
-# print(type(Verfication().GetEmailForVerfication('getahunababu@gmail.com')[0]))
-
-# pas=AddisCheretaUser().GetAddisCheretaUserData('getahunababu@gmail.com')
-# result=bcrypt.checkpw("1234567890".encode('utf-8'),pas)
-# print(pas)
-# print(type(pas)==type([]))
-# print(result!=[])
-# print(AddisCheretaUser().GetAllCheretaUserEmail()!=False)
-
-# print(AddisCheretaUser().GetCheretaUserDataJson()!=False)
-
-# print(Item().InsertItemTable(2,"Yabsera","Yabsera","Yabsera","Yabsera",1,"2024-12-25")==True)
-# print(type(list(Item().GetItemWithIID((str(758424)))))==type([]))
-
-# print(Item().GetAllItemLimit()!=False)
-
-# print(Item().Search(Price="",Name="",Category="Art"))
-
-# print(Buyer().InsertBuyerTable(1,'758424','2024-12-20',600))
-# print(Item().GetItemWithIID('758424'))
-
-# print(Buyer().HighestBid(316197))
-# print(Item().ListOfAuction()[0])
-
-# print(Buyer().GetUserBidWithUserId("2"))
-
-# lst=[]
-# for i in Item().UserItemDashboardWithUID("1"):
-#     if Buyer().HighestBid(i[5])==None:
-#         print(list(i)+[0])
-#     else:
-#         print(list(i)+[Buyer().HighestBid(i[5])])
-
-
-# print(Buyer().HighestBidWinnerWithId('758424'))
-# print(AddisCheretaUser().GetPhonenumberEmailWithUID(Item().GetItemWithIID('381383')[1]))
-# print(AddisCheretaUser().GetPhonenumberEmailWithUID(Buyer().HighestBidWinnerWithId('758424')[1]))
-
-# print("Rating Count Of Rating Of User",Rating().CountOfRatingOfUser(2))
-# print("Rating Sum Rating Of User",Rating().SumRatingOfUser(2))
-# print("Rating Min Rating Of User",Rating().MinRatingOfUser(2))
-# print("Rating Average Rating Of User",Rating().AverageRatingOfUser(2))
-# print("Rating Bayesian Rating Of User",Rating().BayesianRatingOfUser(2))
-# print(AddisCheretaUser().GetPhotoWithUID(1))
+print(Database().AllowedFile("filename.png"))
+print(Database().AllowedFile("filename.jpg"))
+print(Database().AllowedFile("filename.jpeg"))
+print(Database().AllowedFile("filename.txt"))
+print(AddisCheretaUser().ResetPassWordForUser('getahunababu@gmail.com','654321'))
+print(type(Verfication().GetEmailForVerfication('getahunababu@gmail.com')[0]))
+pas=AddisCheretaUser().GetAddisCheretaUserData('getahunababu@gmail.com')
+result=bcrypt.checkpw("1234567890".encode('utf-8'),pas)
+print(pas)
+print(type(pas)==type([]))
+print(result!=[])
+print(AddisCheretaUser().GetAllCheretaUserEmail()!=False)
+print(AddisCheretaUser().GetCheretaUserDataJson()!=False)
+print(Item().InsertItemTable(2,"Yabsera","Yabsera","Yabsera","Yabsera",1,"2024-12-25")==True)
+print(type(list(Item().GetItemWithIID((str(758424)))))==type([]))
+print(Item().GetAllItemLimit()!=False)
+print(Item().Search(Price="",Name="",Category="Art"))
+print(Buyer().InsertBuyerTable(1,'758424','2024-12-20',600))
+print(Item().GetItemWithIID('758424'))
+print(Buyer().HighestBid(316197))
+print(Item().ListOfAuction()[0])
+print(Buyer().GetUserBidWithUserId("2"))
+lst=[]
+for i in Item().UserItemDashboardWithUID("1"):
+    if Buyer().HighestBid(i[5])==None:
+        print(list(i)+[0])
+    else:
+        print(list(i)+[Buyer().HighestBid(i[5])])
+print(Buyer().HighestBidWinnerWithId('758424'))
+print(AddisCheretaUser().GetPhonenumberEmailWithUID(Item().GetItemWithIID('381383')[1]))
+print(AddisCheretaUser().GetPhonenumberEmailWithUID(Buyer().HighestBidWinnerWithId('758424')[1]))
+print("Rating Count Of Rating Of User",Rating().CountOfRatingOfUser(2))
+print("Rating Sum Rating Of User",Rating().SumRatingOfUser(2))
+print("Rating Min Rating Of User",Rating().MinRatingOfUser(2))
+print("Rating Average Rating Of User",Rating().AverageRatingOfUser(2))
+print("Rating Bayesian Rating Of User",Rating().BayesianRatingOfUser(2))
+print(AddisCheretaUser().GetPhotoWithUID(1))
